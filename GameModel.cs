e62829d8af0dddfe80f185c1025fbc4861c5d054
Yura Perov <thetmgaming36@mail.ru>
2021-04-05 02:07:08 +0500

    Create base GameModel

---
+++ GameModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Top_Down_shooter
+{
+    class GameModel
+    {
+        private Player player;
+
+        public Point PositionPlayer => player.Position;
+        public Size ScalePlayer => player.Scale;
+        public Bitmap AtlasAnimationsPlayer => player.AtlasAnimations;
+
+        public GameModel()
+        {
+            player = new Player();
+        }
+
+         
+    }
+}
--- Player.cs
+++ Player.cs
@@ -9,12 +9,12 @@ namespace Top_Down_shooter
 
     class Player
     {
-        public int X { get; set; }
-        public int Y { get; set; }
+        public Point Position { get; set; }
         public int Speed { get; set; }
         public int Health { get; set; }
         public Direction Direction { get; set; }
         public Bitmap AtlasAnimations { get; private set; }
+        public Size Scale { get; set; }
 
         public Player()
         {
