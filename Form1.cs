acbd4f86907f7845c871d1a38b1e7b83e3577640
Yura Perov <thetmgaming36@mail.ru>
2021-04-07 12:08:08 +0500

    Project was cleared

--- Form1.cs
+++ Form1.cs
@@ -48,53 +48,23 @@ namespace Top_Down_shooter
         {
             Graphics g = e.Graphics;
 
-            g.DrawImage(
-                gameModel.AtlasAnimationsPlayer, 
-                gameModel.PlayerX, gameModel.PlayerY, 
-                new Rectangle(new Point(gameModel.ScalePlayer.Width * currentFrameAnimation, 
-                                        gameModel.ScalePlayer.Height * (int)GetAnimationType(gameModel.DirectionXPlayer, gameModel.DirectionYPlayer, gameModel.SightPlayer)), 
-                              gameModel.ScalePlayer),
-                GraphicsUnit.Pixel);
+            
 
         }
 
         protected override void OnKeyDown(KeyEventArgs e)
         {
-            switch (e.KeyCode)
-            {
-                case Keys.W:
-                    gameModel.ChangeDirectionPlayer(DirectionY.Up);
-                    break;
-                case Keys.A:
-                    gameModel.ChangeDirectionPlayer(DirectionX.Left);
-                    break;
-                case Keys.S:
-                    gameModel.ChangeDirectionPlayer(DirectionY.Down);
-                    break;
-                case Keys.D:
-                    gameModel.ChangeDirectionPlayer(DirectionX.Right);
-                    break;
-            }
+            
         }
 
         protected override void OnKeyUp(KeyEventArgs e)
         {
-            switch (e.KeyCode)
-            {
-                case Keys.W:
-                case Keys.S:
-                    gameModel.ChangeDirectionPlayer(DirectionY.Idle);
-                    break;
-                case Keys.A:
-                case Keys.D:
-                    gameModel.ChangeDirectionPlayer(DirectionX.Idle);
-                    break;
-            }
+            
         }
 
         public void UpdateGameLoop(object sender, EventArgs args)
         {
-            gameModel.MovePlayer();
+            
             Invalidate();
         }
 
--- GameModel.cs
+++ GameModel.cs
@@ -9,36 +9,11 @@ namespace Top_Down_shooter
 {
     class GameModel
     {
-        private Player player;
-
-        public Bitmap AtlasAnimationsPlayer => player.AtlasAnimations;
-        public int PlayerX => player.X;
-        public int PlayerY => player.Y;
-        public Size ScalePlayer => player.Scale;
-        public DirectionX DirectionXPlayer => player.DirectionX;
-        public DirectionY DirectionYPlayer => player.DirectionY;
-        public Sight SightPlayer => player.Sight;
+        
 
         public GameModel()
         {
-            player = new Player();
+           
         }
-
-        public void MovePlayer()
-        {
-            player.X += player.Speed * (int)player.DirectionX;
-
-            player.Y += player.Speed * (int)player.DirectionY;
-        }
-
-        public void ChangeDirectionPlayer(DirectionX directionX)
-        {
-            player.DirectionX = directionX;
-            if (player.DirectionX != DirectionX.Idle)
-                player.Sight = directionX == DirectionX.Left ? Sight.Left : Sight.Right;
-        }
-        
-        public void ChangeDirectionPlayer(DirectionY directionY) => player.DirectionY = directionY;
-
     }
 }
