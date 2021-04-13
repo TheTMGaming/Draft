84915eb9c5f19b35034f22900864ca1406922235
Yura Perov <thetmgaming36@mail.ru>
2021-04-13 09:47:46 +0500

    Added method Draw and Image's positions

--- UI.cs
+++ UI.cs
@@ -6,23 +6,41 @@ namespace Top_Down_shooter
     {
         public int Percent { get; }
 
-        private int x, y;
-        public readonly Bitmap BackgroundImage;
-        public readonly Bitmap BarImage;
-        public readonly Bitmap HeartImage;
+        private readonly int x, y;
+        private readonly Bitmap backgroundImage;
+        private readonly Bitmap barImage;
+        private readonly Bitmap heartImage;
+
+        private readonly Point offsetBackground;
+        private readonly Point offsetBar;
 
         public HealthBar(int x, int y)
         {
             this.x = x;
             this.y = y;
-            BackgroundImage = new Bitmap(@"Sprites/BackgroundHealthBar.png");
-            BarImage = new Bitmap(@"Sprites/HealthBar.png");
-            HeartImage = new Bitmap(@"Sprites/HeartHealthBar.png");
-        }
+            backgroundImage = new Bitmap(@"Sprites/BackgroundHealthBar.png");
+            barImage = new Bitmap(@"Sprites/HealthBar.png");
+            heartImage = new Bitmap(@"Sprites/Heart.png");
+
+            offsetBackground = new Point(45, 45);
+            offsetBar = new Point(offsetBackground.X + 2, offsetBackground.Y + 3);
+    }
 
         public void Draw(Graphics g)
         {
-            g.DrawImage(HeartImage, x, y, new Rectangle(new Point(0, 0), new Size(HeartImage.Width, HeartImage.Height)),
+            g.DrawImage(
+                backgroundImage, x + offsetBackground.X, y + offsetBackground.Y,
+                new Rectangle(new Point(0, 0), new Size(backgroundImage.Width, backgroundImage.Height)),
+                GraphicsUnit.Pixel);
+
+            g.DrawImage(
+                barImage, x + offsetBar.X, y + offsetBar.Y,
+                new Rectangle(new Point(0, 0), new Size(barImage.Width, barImage.Height)),
+                GraphicsUnit.Pixel);
+
+            g.DrawImage(
+                heartImage, x, y,
+                new Rectangle(new Point(0, 0), new Size(heartImage.Width, heartImage.Height)),
                 GraphicsUnit.Pixel);
         }
         
