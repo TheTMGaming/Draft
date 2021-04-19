140f330676ff8f0ee6328182f55c3085da38bb91
Yura Perov <thetmgaming36@mail.ru>
2021-04-19 23:56:57 +0500

    Refactored Character render: StateCount and FrameCount's setters to public

---
+++ Scripts/GameRender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Top_Down_shooter.Scripts
+{
+    class GameRender
+    {
+
+    }
+}
