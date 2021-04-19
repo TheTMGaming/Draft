8a80e8a43bca4851903a4653d1b9947ed85e87f0
Yura Perov <thetmgaming36@mail.ru>
2021-04-19 13:34:46 +0500

    Init class QuadTree and created method Split

---
+++ Scripts/QuadTree.cs
@@ -0,0 +1,45 @@
+﻿using System.Drawing;
+
+namespace Top_Down_shooter
+{
+    class QuadTree
+    {
+        private readonly Rectangle bounds;
+        private readonly int depth;
+        private readonly Rectangle[] objects;
+        private readonly QuadTree[] nodes;
+
+        private static int maxObjectsCount = 10;
+        private static int maxLevels = 4;
+
+        public QuadTree(Rectangle bounds, int nextDepth)
+        {
+            this.bounds = bounds;
+            depth = nextDepth;
+            nodes = new QuadTree[4];
+        }
+
+        private void Split()
+        {
+            var subWidth = bounds.Width / 2;
+            var subHeight = bounds.Height / 2;
+
+            nodes[0] = new QuadTree(
+                new Rectangle(bounds.X + subWidth, bounds.Y, subWidth, subHeight),
+                depth + 1);
+
+            nodes[1] = new QuadTree(
+                new Rectangle(bounds.X, bounds.Y, subWidth, subHeight),
+                depth + 1);
+
+            nodes[2] = new QuadTree(
+                new Rectangle(bounds.X, bounds.Y + subHeight, subWidth, subHeight),
+                depth + 1);
+
+            nodes[3] = new QuadTree(
+                new Rectangle(bounds.X + subWidth, bounds.Y + subHeight, subWidth, subHeight),
+                depth + 1);
+        }
+
+    }
+}
