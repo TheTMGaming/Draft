b2a459967fd47de678a1963673ea1ab2367acfb2
Yura Perov <thetmgaming36@mail.ru>
2021-04-20 15:57:22 +0500

    Created Bullets render

---
+++ Scripts/Renders/BulletsRender.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Top_Down_shooter.Scripts.GameObjects;
+
+namespace Top_Down_shooter.Scripts.Renders
+{
+    class BulletsRender : IRender
+    {
+        private readonly LinkedList<Bullet> bullets;
+        private readonly Bitmap image;
+
+        public BulletsRender(LinkedList<Bullet> bullets, Bitmap image)
+        {
+            this.bullets = bullets;
+            this.image = image;
+        }
+
+        public void Draw(Graphics g)
+        {
+            foreach (var bullet in bullets)
+            {
+                g.TranslateTransform(bullet.X, bullet.Y);
+                g.RotateTransform((float)(bullet.Angle * 180 / Math.PI));
+                g.TranslateTransform(-bullet.X, -bullet.Y);
+
+                g.DrawImage(image,
+                    bullet.X - image.Width / 2, bullet.Y - image.Height / 2,
+                    new Rectangle(0, 0, image.Width, image.Height),
+                    GraphicsUnit.Pixel);
+
+                g.ResetTransform();
+            }
+        }
+    }
+}
