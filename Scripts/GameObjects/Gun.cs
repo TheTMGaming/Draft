d4790d08fa2491be85a116a13fdda0a65192e155
Yura Perov <thetmgaming36@mail.ru>
2021-05-02 22:29:40 +0500

    Added Gun's cooldown

--- Scripts/GameObjects/Gun.cs
+++ Scripts/GameObjects/Gun.cs
@@ -4,12 +4,15 @@ namespace Top_Down_shooter.Scripts.GameObjects
 {
     class Gun : GameObject
     {
+        public int Cooldown { get; set; }
+
         public float Angle { get; set; }
 
         public readonly Point SpawnBullets = new Point(10, -10);
 
         public Gun(int x, int y)
         {
+            Cooldown = 30;
             X = x;
             Y = y;
         }
