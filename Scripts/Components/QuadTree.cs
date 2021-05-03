6fa8befb3fc01a9825cec4eddf8df27a11254934
Yura Perov <thetmgaming36@mail.ru>
2021-05-03 22:57:45 +0500

    Updated TileMapController and renamed to Map

---
+++ Scripts/Components/QuadTree.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using Top_Down_shooter.Scripts.GameObjects;
+
+namespace Top_Down_shooter.Scripts.Controllers
+{
+    class QuadTree
+    {
+        private readonly Rectangle bounds;
+        private readonly int depth;
+
+        private List<GameObject> objects;
+        private readonly List<QuadTree> nodes;
+
+        private readonly int maxObjectsCount = 10;
+        private readonly int maxDepth = 4;
+
+        public QuadTree(Rectangle bounds, int nextDepth = 0)
+        {
+            this.bounds = bounds;
+            depth = nextDepth;
+
+            nodes = new List<QuadTree>();
+            objects = new List<GameObject>();
+        }
+
+        public List<GameObject> GetCandidateToCollision(GameObject gameObject)
+        {
+            var returnedList = new List<GameObject>(objects);
+
+            if (nodes.Count > 0)
+            return returnedList.Concat(
+                GetContainedNodes(gameObject)
+                 .SelectMany(node => node.GetCandidateToCollision(gameObject))
+                )
+                .Distinct()
+                .ToList();
+
+            return returnedList;
+        }
+
+        public void Insert(GameObject gameObject)
+        {
+            if (nodes.Count > 0)
+            {
+                foreach (var node in GetContainedNodes(gameObject))
+                    node.Insert(gameObject);
+
+                return;
+            }
+
+            objects.Add(gameObject);
+
+            if (objects.Count > maxObjectsCount && depth < maxDepth)
+            {
+                if (nodes.Count == 0)
+                    Split();
+
+                foreach (var obj in objects)
+                {
+                    foreach (var node in GetContainedNodes(gameObject))
+                        node.Insert(gameObject);
+                }
+
+                objects = new List<GameObject>();
+            }
+        }
+
+        public void Clear()
+        {
+            objects = new List<GameObject>();
+
+            foreach (var node in nodes)
+                node.Clear();
+        }
+
+        private void Split()
+        {
+            var subWidth = bounds.Width / 2;
+            var subHeight = bounds.Height / 2;
+
+            nodes.Add(new QuadTree(
+                new Rectangle(bounds.X + subWidth, bounds.Y, subWidth, subHeight),
+                depth + 1));
+
+            nodes.Add(new QuadTree(
+                new Rectangle(bounds.X, bounds.Y, subWidth, subHeight),
+                depth + 1));
+
+            nodes.Add(new QuadTree(
+                new Rectangle(bounds.X, bounds.Y + subHeight, subWidth, subHeight),
+                depth + 1));
+
+            nodes.Add(new QuadTree(
+                new Rectangle(bounds.X + subWidth, bounds.Y + subHeight, subWidth, subHeight),
+                depth + 1));
+        }
+
+        private List<QuadTree> GetContainedNodes(GameObject gameObject)
+        {
+            var list = new List<QuadTree>();
+
+            var verticalMidpoint = bounds.X + bounds.Width / 2;
+            var horizontalMidpoint = bounds.Y + bounds.Height / 2;
+
+            var rect = new Rectangle(
+                gameObject.X - gameObject.Size.Width / 2,
+                gameObject.Y - gameObject.Size.Height / 2,
+                gameObject.Size.Width, gameObject.Size.Height);
+
+            if (rect.Y < horizontalMidpoint && rect.X + rect.Width > verticalMidpoint)
+                list.Add(nodes[0]);
+
+            if (rect.X < verticalMidpoint && rect.Y < horizontalMidpoint)
+                list.Add(nodes[1]);
+
+            if (rect.X < verticalMidpoint && rect.Y + rect.Height > horizontalMidpoint)
+                list.Add(nodes[2]);
+
+            if (rect.X + rect.Width > verticalMidpoint && rect.Y + rect.Height > horizontalMidpoint)
+                list.Add(nodes[3]);
+
+            return list;
+        }
+    }
+}
---
+++ Scripts/Controllers/Map.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Drawing;
+using Top_Down_shooter.Scripts.GameObjects;
+using Top_Down_shooter.Scripts.Renders;
+using Top_Down_shooter.Properties;
+
+namespace Top_Down_shooter.Scripts.Controllers
+{
+    enum TileTypes
+    {
+        Grass, Box
+    }
+
+    class Map
+    {
+        public readonly Point PositionPlayer;
+        public readonly Point PositionBoss;
+        public readonly Point[] PositionsEnemies;
+        public readonly SpriteRender[,] Tiles;
+
+        private readonly int width;
+        private readonly int height;
+
+        private readonly int sizeTile = int.Parse(Resources.TileSize);
+        private readonly Random randGenerator = new Random();
+       
+        public Map()
+        {
+            width = int.Parse(Resources.MapWidth);
+            height = int.Parse(Resources.MapHeight);
+            Tiles = new SpriteRender[width / sizeTile, height / sizeTile];
+
+            PositionBoss = new Point(
+                randGenerator.Next(width / 2 - 20, width / 2 + 20 + 1),
+                randGenerator.Next(height / 2 - 20, height / 2 + 20 + 1));
+
+            PositionPlayer = new Point(
+                randGenerator.Next(sizeTile + 20, width / 2 - 20) + width / 2 * randGenerator.Next(0, 2),
+                randGenerator.Next(sizeTile + 20, height / 2 - 20) + height / 2 * randGenerator.Next(0, 2));
+            
+
+            CreateTiles();
+
+        }
+
+        private void CreateTiles()
+        {
+            var rand = new Random();
+
+            for (var x = 0; x < width / sizeTile; x += 1)
+            {
+                for (var y = 0; y < height / sizeTile; y += 1)
+                {
+
+                    if (rand.NextDouble() > .95)
+                    {
+                        var box = new SpriteRender(x * sizeTile, y * sizeTile, Resources.Box);
+                        Tiles[x, y] = box;
+                        Physics.AddToTrackingCollisions(new GameObject
+                        {
+                            X = box.X + box.Size.Width / 2,
+                            Y = box.Y + box.Size.Height / 2,
+                            Size = box.Size
+                        });
+                    }
+                    else
+                    {
+                        Tiles[x, y] = new SpriteRender(x * sizeTile, y * sizeTile,
+                            Resources.Grass.Extract(new Rectangle(sizeTile * rand.Next(0, 4), 0, sizeTile, sizeTile)));
+                    }
+                }
+            }
+        }
+
+        public void DrawTiles(Graphics g)
+        {
+            foreach (var tile in Tiles)
+            {
+                tile.Draw(g);
+            }
+        }
+
+    }
+}
