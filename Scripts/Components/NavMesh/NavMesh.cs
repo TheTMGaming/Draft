0a0e0af0153c76f5bf34502335eba3e6c29b291c
Yura Perov <thetmgaming36@mail.ru>
2021-05-19 01:54:49 +0500

    Added Enemy:

---
+++ Scripts/Components/NavMesh/NavMesh.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Top_Down_shooter.Scripts.Components
+{
+    static class NavMesh
+    {
+        static NavMesh()
+        {
+
+        }
+    }
+}
