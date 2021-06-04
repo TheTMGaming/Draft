3670d4282889ab19a8116bf0dd86ceda65e11856
Yura Perov <thetmgaming36@mail.ru>
2021-06-04 21:26:35 +0500

    Fixed font

--- Scripts/Source/Program.cs
+++ Scripts/Source/Program.cs
@@ -1,19 +1,57 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing.Text;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Top_Down_shooter
 {
+
     static class Program
     {
+        [DllImport("gdi32", EntryPoint = "AddFontResource")]
+        public static extern int AddFontResourceA(string lpFileName);
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        private static extern int AddFontResource(string lpszFilename);
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        private static extern int CreateScalableFontResource(uint fdwHidden, string
+            lpszFontRes, string lpszFontFile, string lpszCurrentPath);
+
         [STAThread]
         static void Main()
         {
+            RegisterFont("Intro.otf");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+        }
+
+        private static void RegisterFont(string contentFontName)
+        {
+            // Creates the full path where your font will be installed
+            var fontDestination = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Fonts), contentFontName);
+
+            if (!File.Exists(fontDestination))
+            {
+                // Copies font to destination
+                System.IO.File.Copy(Path.Combine(System.IO.Directory.GetCurrentDirectory(), contentFontName), fontDestination);
+
+                // Retrieves font name
+                // Makes sure you reference System.Drawing
+                PrivateFontCollection fontCol = new PrivateFontCollection();
+                fontCol.AddFontFile(fontDestination);
+                var actualFontName = fontCol.Families[0].Name;
+
+                //Add font
+                AddFontResource(fontDestination);
+                //Add registry entry   
+                Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts", actualFontName, contentFontName, RegistryValueKind.String);
+            }
         }
     }
 }
